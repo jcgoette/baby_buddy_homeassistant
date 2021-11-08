1f6a3e12d05b29c42acc392d3172cb7ce9d806c7
jcgoette <jcgoette@gmail.com>
2021-11-08 09:46:20 -0500

    ABC

--- custom_components/babybuddy/const.py
+++ custom_components/babybuddy/const.py
@@ -21,9 +21,9 @@ from homeassistant.helpers.config_validation import time_period_str
 
 DOMAIN: Final = "babybuddy"
 
-CONF_WEIGHT_UNIT: Final = "weight"
-CONF_TEMPERATURE_UNIT: Final = "temperature"
 CONF_FEEDING_UNIT: Final = "feedings"
+CONF_TEMPERATURE_UNIT: Final = "temperature"
+CONF_WEIGHT_UNIT: Final = "weight"
 
 DEFAULT_NAME: Final = "Baby Buddy"
 DEFAULT_PORT: Final = 8000
@@ -59,8 +59,11 @@ ATTR_TYPE: Final = "type"
 ATTR_WEIGHT: Final = "weight"
 ATTR_WET: Final = "wet"
 
-FEEDING_TYPE: Final = "feeding_type"
-FEEDING_TYPES: Final = ["Breast milk", "Formula", "Fortified breast milk", "Solid food"]
+DEFAULT_DIAPER_TYPE: Final = ATTR_WET
+DIAPER_COLOR: Final = "diaper_color"
+DIAPER_COLORS: Final = ["Black", "Brown", "Green", "Yellow"]
+DIAPER_TYPE: Final = "change_type"
+DIAPER_TYPES: Final = ["Wet", "Solid", "Wet and Solid"]
 FEEDING_METHOD: Final = "feeding_method"
 FEEDING_METHODS: Final = [
     "Bottle",
@@ -70,11 +73,8 @@ FEEDING_METHODS: Final = [
     "Parent fed",
     "Self fed",
 ]
-DIAPER_TYPE: Final = "change_type"
-DIAPER_TYPES: Final = ["Wet", "Solid", "Wet and Solid"]
-DIAPER_COLOR: Final = "diaper_color"
-DIAPER_COLORS: Final = ["Black", "Brown", "Green", "Yellow"]
-DEFAULT_DIAPER_TYPE: Final = ATTR_WET
+FEEDING_TYPE: Final = "feeding_type"
+FEEDING_TYPES: Final = ["Breast milk", "Formula", "Fortified breast milk", "Solid food"]
 
 
 @dataclass
@@ -86,57 +86,56 @@ class BabyBuddyEntityDescription(SensorEntityDescription, SwitchEntityDescriptio
 
 SENSOR_TYPES: tuple[BabyBuddyEntityDescription, ...] = (
     BabyBuddyEntityDescription(
-        key=ATTR_CHANGES,
-        state_key=ATTR_TIME,
         device_class=DEVICE_CLASS_TIMESTAMP,
         icon="mdi:paper-roll-outline",
+        key=ATTR_CHANGES,
+        state_key=ATTR_TIME,
     ),
     BabyBuddyEntityDescription(
+        icon="mdi:baby-bottle-outline",
         key=ATTR_FEEDINGS,
         state_class=STATE_CLASS_MEASUREMENT,
         state_key=ATTR_AMOUNT,
-        icon="mdi:baby-bottle-outline",
     ),
     BabyBuddyEntityDescription(
-        key=ATTR_NOTES,
-        state_key=ATTR_TIME,
         device_class=DEVICE_CLASS_TIMESTAMP,
         icon="mdi:note-multiple-outline",
+        key=ATTR_NOTES,
+        state_key=ATTR_TIME,
     ),
     BabyBuddyEntityDescription(
+        icon="mdi:sleep",
         key=ATTR_SLEEP,
+        native_unit_of_measurement=TIME_MINUTES,
         state_key=lambda value: int(
             time_period_str(value[ATTR_DURATION]).total_seconds() / 60
         ),
-        native_unit_of_measurement=TIME_MINUTES,
-        icon="mdi:sleep",
     ),
     BabyBuddyEntityDescription(
-        key=ATTR_TEMPERATURE,
-        state_key=ATTR_TEMPERATURE,
         device_class=DEVICE_CLASS_TEMPERATURE,
         icon="mdi:thermometer",
+        key=ATTR_TEMPERATURE,
         state_class=STATE_CLASS_MEASUREMENT,
         state_key=ATTR_TEMPERATURE,
     ),
     BabyBuddyEntityDescription(
+        icon="mdi:baby",
         key=ATTR_TUMMY_TIMES,
         state_key=lambda value: int(
             time_period_str(value[ATTR_DURATION]).total_seconds() / 60
         ),
-        icon="mdi:baby",
     ),
     BabyBuddyEntityDescription(
-        key=ATTR_WEIGHT,
-        state_key=ATTR_WEIGHT,
-        state_class=STATE_CLASS_MEASUREMENT,
         icon="mdi:scale-bathroom",
+        key=ATTR_WEIGHT,
+        state_class=STATE_CLASS_MEASUREMENT,
+        state_key=ATTR_WEIGHT,
     ),
     BabyBuddyEntityDescription(
-        key=ATTR_TIMERS,
-        state_key=ATTR_START,
         device_class=DEVICE_CLASS_TIMESTAMP,
         icon="mdi:timer-sand",
+        key=ATTR_TIMERS,
+        state_key=ATTR_START,
     ),
 )
 
@@ -150,29 +149,29 @@ class BabyBuddySelectDescription(SelectEntityDescription):
 
 SELECTOR_TYPES: tuple[BabyBuddySelectDescription, ...] = (
     BabyBuddySelectDescription(
+        icon="mdi:baby-bottle-outline",
         key=FEEDING_METHOD,
         name=f"{DEFAULT_NAME} Feeding method",
         options=FEEDING_METHODS,
-        icon="mdi:baby-bottle-outline",
     ),
     BabyBuddySelectDescription(
+        icon="mdi:baby-bottle-outline",
         key=FEEDING_TYPE,
         name=f"{DEFAULT_NAME} Feeding type",
         options=FEEDING_TYPES,
-        icon="mdi:baby-bottle-outline",
     ),
     BabyBuddySelectDescription(
+        icon="mdi:paper-roll-outline",
         key=DIAPER_TYPE,
         name=f"{DEFAULT_NAME} Diaper type",
         options=DIAPER_TYPES,
-        icon="mdi:paper-roll-outline",
     ),
     BabyBuddySelectDescription(
+        icon="mdi:paper-roll-outline",
         key=DIAPER_COLOR,
         name=f"{DEFAULT_NAME} Diaper color",
         options=DIAPER_COLORS,
-        icon="mdi:paper-roll-outline",
     ),
 )
 
-PLATFORMS: Final = ["sensor", "switch", "select"]
+PLATFORMS: Final = ["sensor", "select", "switch"]
