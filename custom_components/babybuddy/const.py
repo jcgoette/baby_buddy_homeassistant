dbdb8b671be537cbf4fc082d42d9150e38d781a7
jcgoette <jcgoette@gmail.com>
2022-10-27 21:51:54 -0400

    refactor: Remove unused import

--- custom_components/babybuddy/const.py
+++ custom_components/babybuddy/const.py
@@ -2,7 +2,7 @@
 from __future__ import annotations
 
 from collections.abc import Callable
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 from typing import Final
 
 import homeassistant.util.dt as dt_util
