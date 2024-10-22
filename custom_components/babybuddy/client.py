61b3718076cf3bd5ba0d7ad2acf0557fda1489ee
jcgoette <jcgoette@gmail.com>
2024-10-22 12:07:15 +0000

    fix: If tzinfo already present, do not override

--- custom_components/babybuddy/client.py
+++ custom_components/babybuddy/client.py
@@ -133,7 +133,8 @@ def get_datetime_from_time(value: datetime | time) -> datetime:
     if isinstance(value, time):
         value = datetime.combine(dt_util.now().date(), value, dt_util.DEFAULT_TIME_ZONE)
     if isinstance(value, datetime):
-        value = value.replace(tzinfo=dt_util.DEFAULT_TIME_ZONE)
+        if value.tzinfo is None:
+            value = value.replace(tzinfo=dt_util.DEFAULT_TIME_ZONE)
     if value > dt_util.now():
         raise ValidationError("Time cannot be in the future.")
     return value
